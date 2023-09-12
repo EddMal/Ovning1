2fe1a2b2dcbc8bfb306093cbda885cd8814e769f
EddMal <107246914+EddMal@users.noreply.github.com>
2023-09-12 16:37:27 +0200

    Not quite perfext but better.

--- Ovning1/Program.cs
+++ Ovning1/Program.cs
@@ -4,15 +4,17 @@ namespace Ovning1
 {
     internal class Program
     {
-        public static string[] EmployeeName;
-        public static int[] EmployeeWage;
-        public static int EmployeeID;
+        public const int maxNumberOfEmployees = 100;
+        public const int maxNumberLetters = 30;
+        public static string[] EmployeeName = new string[maxNumberLetters];
+        public static int[] EmployeeWage = new int[maxNumberOfEmployees];
         public static int numberOfEmployees = 0;
+        public static bool exit = false;
         static void Main(string[] args)
         {
-            static void Main(string[] args)
+            
+            while (exit == false)
             {
-
                 Console.WriteLine("Register Options:");
                 Console.WriteLine("1. Register employee with name and wage.");
                 Console.WriteLine("2. Print list.");
@@ -20,49 +22,42 @@ namespace Ovning1
                 int number = int.Parse(Console.ReadLine());
                 register.SelectOption(number);
             }
-
-
         }
 
 
         class register
         {
             public static void SelectOption(int number)
-            {
-                bool exit = false;
-                while (exit == false)
+            {         
+                switch (number)
                 {
-                    switch (number)
-                    {
-                        case 1:
+                    case 1:
+                        numberOfEmployees++;
 
-                            numberOfEmployees++;
-
-                            Console.WriteLine("Enter emplyees name:");
-                            EmployeeName[EmployeeID] = Console.ReadLine();
-
-                            Console.WriteLine("Enter emplyees wage:");
-                            EmployeeWage[EmployeeID] = int.Parse(Console.ReadLine());
-
-                            break;
-                        case 2:
-                            Console.WriteLine("List of employees:");
-                            for(var NumberEmployees = 0; NumberEmployees == numberOfEmployees; NumberEmployees--)
-                            {
-                                Console.WriteLine("Employee ID: {0}", NumberEmployees);
-                                Console.WriteLine(EmployeeName);
-                                Console.WriteLine("Wage: {0}", EmployeeWage);
-                            }
-                            break;
-                        case 3:
-                            exit = false;
-
-                            break;
-                        default:
-                            break;
-                    }
-                   
+                        Console.WriteLine("Enter emplyees name:");
+                        EmployeeName[numberOfEmployees] = Console.ReadLine();
+                        Console.WriteLine("Enter emplyees wage:");
+                        EmployeeWage[numberOfEmployees] = int.Parse(Console.ReadLine());
+                        break;
+                    case 2:
+                        Console.WriteLine("List of employees:");
+                        for(var NumberEmployees = numberOfEmployees; NumberEmployees > 0; NumberEmployees--)
+                        {
+                            Console.WriteLine("Employee ID: {0}", NumberEmployees);
+                            Console.WriteLine("Name : {0}", EmployeeName[NumberEmployees]);
+                            Console.WriteLine("Wage: {0}", EmployeeWage[NumberEmployees]);
+                        }
+                        break;
+                    case 3:
+                        exit = true;
+                        break;
+                    default:
+                        break;
                 }
+
+                Console.WriteLine("Press enter to continue.");
+                Console.ReadLine();
+
             }
 
         }
