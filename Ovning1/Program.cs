90f71824ed30dcae57075561c7a68ac19b134381
EddMal <107246914+EddMal@users.noreply.github.com>
2023-09-14 10:26:13 +0200

    Formating 2.

--- Ovning1/Program.cs
+++ Ovning1/Program.cs
@@ -4,10 +4,11 @@ namespace Ovning1
 {
     internal class Program
     {
-
+        // Declare object payRoll of class PayRoll.
         private static PayRoll payRoll;
         static void Main(string[] args)
         {
+            // Use constructor PayRoll(), content which is a list "employees".
             payRoll = new PayRoll();
 
             while (true)
@@ -21,7 +22,7 @@ namespace Ovning1
         {
 
             Console.WriteLine("Register Options:");
-            Console.WriteLine("1. Register employee with name and wage.");
+            Console.WriteLine("1. Register employee with name and salary.");
             Console.WriteLine("2. Print list.");
             Console.WriteLine("3. Exit.");
         }
@@ -30,25 +31,15 @@ namespace Ovning1
         {
             switch (menuInput)
             {
-                case 1:
-                    numberOfEmployees++;
-
-                    Console.WriteLine("Enter emplyees name:");
-                    EmployeeName[numberOfEmployees] = Console.ReadLine();
-                    Console.WriteLine("Enter emplyees wage:");
-                    EmployeeWage[numberOfEmployees] = int.Parse(Console.ReadLine());
+                case "1":
+                    RegisterEmployee();
                     break;
-                case 2:
-                    Console.WriteLine("List of employees:");
-                    for(var NumberEmployees = numberOfEmployees; NumberEmployees > 0; NumberEmployees--)
-                    {
-                        Console.WriteLine("Employee ID: {0}", NumberEmployees);
-                        Console.WriteLine("Name : {0}", EmployeeName[NumberEmployees]);
-                        Console.WriteLine("Wage: {0}", EmployeeWage[NumberEmployees]);
-                    }
+                case "2":
+                    DisplayPayRoll();
+                   
                     break;
-                case 3:
-                    exit = true;
+                case "3":
+                    Environment.Exit(0);
                     break;
                 default:
                     break;
@@ -59,7 +50,68 @@ namespace Ovning1
 
         }
 
-  
+        private static void DisplayPayRoll()
+        {
+            List<Employee> employees = payRoll.GetEmployees();
 
+            Console.WriteLine("List of employees:");
+            foreach (Employee employee in employees)
+            {
+                Console.WriteLine($"Name: {employee.Name} Salary: {employee.Salary}");
+            }
+        }
+
+        private static void RegisterEmployee()
+        {
+            Console.WriteLine("Enter employee's name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter employee's salary: ");
+            int salary = InputSalary();
+            payRoll.AddEmployees(name, salary);
+        }
+
+        private static int InputSalary()
+        {
+            int salary;
+            bool validationPassed = false;
+            do
+            {
+                string salaryInput = Console.ReadLine();
+                (validationPassed, salary) = ValidateNumberInputFromString(salaryInput);
+            } while (validationPassed == false);
+
+            return salary;
+
+        }
+        /// <summary>
+        /// This method validates whether string input consist of numbers only.
+        /// </summary>
+        /// <param>
+        /// <c>numberInput</c> is the string input for validation.
+        /// </param>
+        /// <returns>
+        /// <c>validationPassed</c> True if the input of <c>numberInput</c> consist of 
+        /// numbers only; otherwise returns false. 
+        /// <c>number</c> is the string input converted to int, if string contains
+        /// illegal characters the value defaults to 0.
+        /// </returns>
+        private static (bool, int) ValidateNumberInputFromString(string numberInput)
+        {
+            int number = 0;
+            bool validationPassed = false;
+                //check if number contains valid characters (numbers).
+                if (!int.TryParse(numberInput, out number))
+                {
+                    Console.WriteLine($"\"{numberInput}\" is not a valid input, use numbers only:");
+                }
+                else
+                {
+                    validationPassed = true;
+                }
+            // If parsing failed, number will deafult to 0 and validationPassed set to false,
+            // else number will be assigned the expected int value and validationPassed true.
+
+            return (validationPassed, number);
+        }
     }
 }
